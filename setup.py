807376db345c8c5d1de519dd5a9a7c94a8bc8eae
Stephen Watts <hello@stephen.ac>
2021-11-12 12:31:46 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,34 +2,12 @@ import setuptools
 
 
 setuptools.setup(
-    name="cdk_ec2",
+    name="aws_work_instance",
     version="0.0.1",
-
     description="Simple EC2 instance stack",
-
-    author="author",
-
     install_requires=[
         "aws-cdk.core==1.110.1",
         "aws-cdk.aws-ec2",
     ],
-
     python_requires=">=3.6",
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-
-        "Intended Audience :: Developers",
-
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Utilities",
-
-        "Typing :: Typed",
-    ],
 )
